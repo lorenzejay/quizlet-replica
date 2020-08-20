541a00cbad77b26094280f15d826fc374e590129
Lorenze Jay <Lorenzejay@gmail.com>
2020-08-20 15:07:45 -0700

    Updates

---
+++ precache-manifest.ab414ee701f3aece6d064410bdfd1f23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3ec77ae86a182800b01f5626f00c6cc",
+    "url": "/quizlet-clone/index.html"
+  },
+  {
+    "revision": "fc8fa9808d69ae71b20a",
+    "url": "/quizlet-clone/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dfaf32d1d07bb6b000cc",
+    "url": "/quizlet-clone/static/js/2.1bb2d634.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quizlet-clone/static/js/2.1bb2d634.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc8fa9808d69ae71b20a",
+    "url": "/quizlet-clone/static/js/main.7aaf866d.chunk.js"
+  },
+  {
+    "revision": "cbcf7ad7ff39c8d99ec0",
+    "url": "/quizlet-clone/static/js/runtime-main.1e135ae4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/quizlet-clone/static/media/logo.5d5d9eef.svg"
+  }
+]);
